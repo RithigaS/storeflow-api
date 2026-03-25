bad4caede943f4463a9b1720a65a91f671060ac9
Rithiga.S <rithiga.s@grootan.com>
2026-03-25 13:56:56 +0530

    feat(exception): add AppException and global error handling foundation with tests

--- src/main/java/com/grootan/storeflow/exception/AppException.java
+++ src/main/java/com/grootan/storeflow/exception/AppException.java
@@ -1,17 +1,36 @@
 package com.grootan.storeflow.exception;
 
 import org.springframework.http.HttpStatus;
+import java.util.Map;
 
 public class AppException extends RuntimeException {
 
     private final HttpStatus status;
+    private final Map<String, String> errors;
 
     public AppException(String message, HttpStatus status) {
         super(message);
         this.status = status;
+        this.errors = null;
     }
+
+    public AppException(String message, HttpStatus status, Map<String, String> errors) {
+        super(message);
+        this.status = status;
+        this.errors = errors;
+    }
+
 
     public HttpStatus getStatus() {
         return status;
+    }
+
+
+    public HttpStatus getStatusCode() {
+        return status;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
     }
 }
